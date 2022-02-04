7c8cd3c49acd8e63ebfc427712c913ee5050e164
ziloka <50429450+ziloka@users.noreply.github.com>
2022-02-04 13:17:06 -0500

    Delete buildImage.sh

--- buildImage.sh
+++
@@ -1,3 +0,0 @@
-# https://www.gitpod.io/docs/config-docker#in-the-existing-workspace
-docker build -f .gitpod.Dockerfile -t gitpod-dockerfile-test .
-docker run --mount type=bind,3source=website,target=/workspaces/website -it gitpod-dockerfile-test bash
